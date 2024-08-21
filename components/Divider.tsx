ada917a02907fc7f80c1b6a37f4fc0e66f18bf8d
Fabricio Magalhães <fabricioms.dev@gmail.com>
2024-08-20 21:20:36 -0300

    feat: custom divider

---
+++ components/Divider.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Divider } from "@rneui/themed";
+
+export const CheckoutDivider = () => {
+  return (
+    <Divider
+      style={{ width: "100%" }}
+      color="#ccc"
+      insetType="left"
+      subHeaderStyle={{}}
+      width={1}
+      orientation="horizontal"
+    />
+  );
+};
