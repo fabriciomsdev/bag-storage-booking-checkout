4a2815f6de52c810ad82cfc434fa2455419f6d35
Fabricio Magalhães <fabricioms.dev@gmail.com>
2024-08-20 21:21:59 -0300

    feat: confirmation component

---
+++ components/Confirmation.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Container } from "./Container";
+import { Grid } from "./Grid";
+import { CheckoutState } from "../types/checkout";
+import { Button } from "@rneui/base";
+
+
+export const CheckoutConfirmation = ({ data }: { data: CheckoutState }) => {
+  return (
+    <Container>
+      <Grid.Row>
+        <View>
+          <Text>
+            {Object.entries(data.booking.items).map(([key, value]) => (
+              <Text key={key}>
+                {key}: {value}
+              </Text>
+            ))}
+          </Text>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 18,
+              marginVertical: 3,
+            }}
+          >
+            $ {data.booking?.totalValue}
+          </Text>
+        </View>
+        <View>
+          <Button
+            style={{
+              width: 120,
+              height: 40,
+              borderRadius: 20,
+              overflow: "hidden",
+            }}
+          >
+            Book
+          </Button>
+        </View>
+      </Grid.Row>
+    </Container>
+  );
+};
